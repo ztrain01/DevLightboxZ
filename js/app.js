2bd8debf326a18f3628109342904520444cf38d9
Zander Allen <ztrain01@gmail.com>
2015-02-03 20:43:36 -0700

    prevented nav to new site

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 //Problem: User when clicking on image goes to a dead end
 //Solution: Create an overlay with the large image - Lightbox
 
+$(document).ready(function() {
+// capture click event on image
+// prevent new page from opening on click
+    $('#imageGallery a').on('click', function(event) {
+        event.preventDefault();
+        console.log('event prevented, yo');
+    })
 
 
 
 
-
-
-
-
+});
