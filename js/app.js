9c8dd21fb7e12f12b24982f1fda7f85450ba07ef
Zander Allen <ztrain01@gmail.com>
2015-02-04 19:16:42 -0700

    added overlay and function

--- js/app.js
+++ js/app.js
@@ -4,11 +4,18 @@
 $(document).ready(function() {
 // capture click event on image
 // prevent new page from opening on click
+var $overlay = $('<div id="overlay"></div>');
+    $('body').append($overlay);
+    
     $('#imageGallery a').on('click', function(event) {
         event.preventDefault();
         console.log('event prevented, yo');
+        $($overlay).show();        
     })
-
+    
+    $($overlay).on('click', function(){
+        $($overlay).hide();
+    });
 
 
 
