2c4fa83edddc769e2469d5f0e9d016d8819141cc
Zander Allen <ztrain01@gmail.com>
2015-02-04 19:43:31 -0700

    Lightbox complete

--- js/app.js
+++ js/app.js
@@ -4,15 +4,28 @@
 $(document).ready(function() {
 // capture click event on image
 // prevent new page from opening on click
-var $overlay = $('<div id="overlay"></div>');
-    $('body').append($overlay);
     
+    var $img = $('<img>');
+    var $overlay = $('<div id="overlay"></div>');
+    
+    $('body').append($overlay);
+    $($overlay).append($img);
+    
+    //Shows overlay when user clicks on image
     $('#imageGallery a').on('click', function(event) {
         event.preventDefault();
         console.log('event prevented, yo');
+        
+        var location = $(this).attr('href');
+        console.log(location);
+        
+        $img.attr('src', location);
+        
         $($overlay).show();        
     })
     
+    
+    //Hides overlay when user clicks on it
     $($overlay).on('click', function(){
         $($overlay).hide();
     });
